d70033adc280ac727a8f661dda48900e58208f0d
sugarfreecat <simoesgg02@gmail.com>
2026-09-11 18:09:40 -0300

    chore(core): adiciona dependências e estrutura base de error/DI

---
+++ lib/core/di/injection_container.dart
@@ -0,0 +1,7 @@
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setupDependencies() {
+  // Manual dependency setup will be added here later.
+}
---
+++ lib/core/error/failures.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  const Failure({this.message = ''});
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+class AuthFailure extends Failure {
+  const AuthFailure({super.message = 'Auth failure'});
+}
